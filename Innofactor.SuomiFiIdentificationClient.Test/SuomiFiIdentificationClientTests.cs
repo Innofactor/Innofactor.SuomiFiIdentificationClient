d72058299f2ec111cfa476e31157605437178ae0
HEADQUARTERS\jukka.hyvarinen <jukka.hyvarinen@innofactor.com>
2017-11-13 16:16:50 +0200

    add one more test

--- Innofactor.SuomiFiIdentificationClient.Test/SuomiFiIdentificationClientTests.cs
+++ Innofactor.SuomiFiIdentificationClient.Test/SuomiFiIdentificationClientTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Innofactor.SuomiFiIdentificationClient.Exceptions;
 using Innofactor.SuomiFiIdentificationClient.Support;
 using Innofactor.SuomiFiIdentificationClient.Test.TestSupport;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -12,16 +14,18 @@ namespace Innofactor.SuomiFiIdentificationClient.Test {
   public class SuomiFiIdentificationClientTests {
 
     private readonly SuomiFiIdentificationClient client;
+    private readonly SamlConfig samlConfig;
 
     public SuomiFiIdentificationClientTests() {
       var cookieStorage = new InMemoryEncryptedCookieStorage();
-      var samlConfig = new SamlConfig {
+      samlConfig = new SamlConfig {
         Saml2SSOUrl = "https://testi.apro.tunnistus.fi/idp/profile/SAML2/Redirect/SSO",
         Saml2SLOUrl = "https://testi.apro.tunnistus.fi/idp/profile/SAML2/Redirect/SLO",
         Saml2IdpEntityId = "https://testi.apro.tunnistus.fi/idp1",
         Saml2EntityId = "https://localhost/SAML/2SP",
         Saml2Certificate = "localhost.pfx",
         Saml2IdpCertificate = "apro-test.cer",
+        Saml2CertificateStoreLocation = StoreLocation.CurrentUser
       };
       var store = new EmbeddedCertificateStore();
       client = new SuomiFiIdentificationClient(samlConfig, new AuthStateAccessor(cookieStorage), new RsaShaCrypto(samlConfig, store));
@@ -34,6 +38,18 @@ namespace Innofactor.SuomiFiIdentificationClient.Test {
 
       Assert.IsNotNull(result, "result");
       Assert.IsTrue(result.StartsWith("https://testi.apro.tunnistus.fi/idp/profile/SAML2/Redirect/SSO?SAMLRequest"), "Start of request URL");
+
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ConfigurationErrorsException))]
+    public void Authenticate_CertificateNotFound() {
+
+      var cookieStorage = new InMemoryEncryptedCookieStorage();
+      samlConfig.Saml2IdpCertificate = "not-found.cer";
+
+      var client = new SuomiFiIdentificationClient(samlConfig, new AuthStateAccessor(cookieStorage), new RsaShaCrypto(samlConfig, new CertificateStore(samlConfig)));
+      client.Authenticate("https://localhost:39390", "fi", new RelayState<string>("register", null, "fi"));
 
     }
 
